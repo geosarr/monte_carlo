212f323d1422cc7010d9b4bf3ac41ef46fc9471f
Georges SARR <georges@vmi1022186.contaboserver.net>
2023-01-19 20:46:23 +0100

    format code

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
 from numpy import tan, pi
 
-standard_cauchy_quantile = lambda y: tan(pi * y - 1 / 2)
+def cauchy_quantile(number: float, theta: float) -> float:
+    assert (
+            theta > 0
+    ), f"theta = {theta} is not valid, theta should be strictly positive"
+    return tan(pi * number - 1 / 2) * theta
+    
